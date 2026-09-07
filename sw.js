c36ed3dad37d10ff618e2c50e9bb562d1b746a18
mb302schedule <a79518850@gmail.com>
2026-09-07 16:17:19 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "schedule-302-v2";
+const CACHE_NAME = "schedule-302-v3";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
